04751b4188eacea9d9a238649f679e53f64c5901
sintakticniSladkorcek <ajda.frankovic@gmail.com>
2021-09-21 20:45:31 +0200

    Remove unused code for post and delete requests (ig)

--- social_media_api_calls/instagram_api_calls.py
+++ social_media_api_calls/instagram_api_calls.py
@@ -8,32 +8,17 @@ def call_api(access_token, data_dictionary, method, endpoint, path, mapped_field
     if path != None:
         url += '/' + data_dictionary['endpoint_mapping'][endpoint]['paths'][path]['path']
     
-    if method == 'get':
-        # remove possible duplicates
-        mapped_fields = list(dict.fromkeys(mapped_fields))
+    # remove possible duplicates
+    mapped_fields = list(dict.fromkeys(mapped_fields))
 
-        # concatenate parameters into string
-        temp = ''
-        for field in mapped_fields:
-            temp += field + ','
-        
-        parameters = 'fields=' + temp[:-1]
+    # concatenate parameters into string
+    temp = ''
+    for field in mapped_fields:
+        temp += field + ','
+    
+    parameters = 'fields=' + temp[:-1]
 
-        # call API
-        response = requests.get(f'{url}?{parameters}&access_token={access_token}')
-
-    elif method == 'post':
-        # concatenate parameters into string
-        parameters = ''
-        for field_name in mapped_fields:
-            parameters += field_name + '=' + str(mapped_fields[field_name]) + '&'
-        parameters = parameters[:-1]
-
-        # call API
-        response = requests.post(f'{url}?{parameters}&access_token={access_token}')
-
-    elif method == 'delete':
-        # call API
-        response = requests.post(f'{url}?access_token={access_token}')
+    # call API
+    response = requests.get(f'{url}?{parameters}&access_token={access_token}')
 
     return response
